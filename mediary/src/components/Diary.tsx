f7ede1e93b8b47671be2cc1eb50024a606710375
Lazar Savic <vicsalaki@gmail.com>
2025-08-28 17:55:35 +0200

    Ubacena prva verzija kalendara kod Diary komponente, sredjen render div blokova i sav CSS vezan za kalendar i prikaz

--- mediary/src/components/Diary.tsx
+++ mediary/src/components/Diary.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
+import { supabase } from "../models/supabaseClients";
+
+interface CalendarCell {
+  date: number | null;
+  currentMonth: boolean;
+}
 
 const Diary = () => {
   const { user } = useAuth();
+  const [firstName, setFirstName] = useState("");
+
+  const loadUser = async () => {
+    const { data: userName, error: userNameError } = await supabase
+      .from("users")
+      .select("name")
+      .eq("id", user?.id)
+      .single();
+
+    if (userNameError) {
+      console.error(userNameError);
+      return;
+    }
+
+    setFirstName(userName.name);
+  };
+
+  useEffect(() => {
+    loadUser();
+  });
 
   if (!user) {
     return <Navigate to="/login" replace />;
@@ -15,11 +41,78 @@ const Diary = () => {
   });
   const year = date.getFullYear();
 
+  const closeButtons = document.querySelectorAll(".modal-close-btn");
+
+  const closeBtnRemoveModal = document.querySelector("#closeBtnRemoveModal");
+  closeBtnRemoveModal?.addEventListener("click", () => {
+    closeBtnRemoveModal
+      .closest(".modals-wrapper")
+      ?.classList.add("is-invisible");
+  });
+
+  closeButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.closest(".modals-wrapper")?.classList.add("is-invisible");
+    });
+  });
+
+  const addFeelingButtons = document.querySelectorAll(".is-add");
+
+  addFeelingButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document
+        .querySelector(".is-add-modal-wrapper")
+        ?.classList.remove("is-invisible");
+    });
+  });
+
+  const removeFeelingButtons = document.querySelectorAll(".is-remove");
+
+  removeFeelingButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document
+        .querySelector(".is-remove-modal-wrapper")
+        ?.classList.remove("is-invisible");
+    });
+  });
+
+  const reviewFeelingButtons = document.querySelectorAll(".is-menu");
+
+  reviewFeelingButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document
+        .querySelector(".is-menu-modal-wrapper")
+        ?.classList.remove("is-invisible");
+    });
+  });
+
+  function generateCalendar(year: number, month: number): CalendarCell[] {
+    const firstDay = new Date(year, month, 1);
+    const lastDay = new Date(year, month + 1, 0);
+    let startDay = firstDay.getDay();
+    startDay = startDay === 0 ? 6 : startDay - 1;
+    const daysInMonth = lastDay.getDate();
+    const totalCells = 35;
+
+    const cells: CalendarCell[] = [];
+
+    for (let i = 0; i < totalCells; i++) {
+      const dayNumber = i - startDay + 1;
+      if (dayNumber > 0 && dayNumber <= daysInMonth) {
+        cells.push({ date: dayNumber, currentMonth: true });
+      } else {
+        cells.push({ date: null, currentMonth: false });
+      }
+    }
+
+    return cells;
+  }
+
   return (
     <>
       <div className="padding-global">
         <div className="container">
-          <h1 className="margin-b-128">{user.email}'s mental health diary</h1>
+          <h1 className="margin-b-128">{firstName}'s mental health diary</h1>
           <div className="diary-wrapper margin-b-160">
             <span className="diary-month-year">
               {month}, {year}.
@@ -35,261 +128,150 @@ const Diary = () => {
                 <span className="day-name">Sunday</span>
               </div>
               <div className="days-grid">
-                <div className="day-block is-top-left-corner">
-                  <div className="add-btn">
+                {/* Days Modals */}
+                <div className="modals-wrapper is-add-modal-wrapper is-invisible">
+                  <div className="modal is-add-modal">
+                    <div className="modal-close-btn">
+                      <img
+                        src="../assets/modal-x.svg"
+                        alt="Modal Close Button"
+                      />
+                    </div>
+
+                    <div className="modal-group">
+                      <p>How are you feeling today?</p>
+                      <input type="text" className="form-field form-width" />
+                    </div>
+
+                    <div className="modal-group">
+                      <p>Describe it by the color</p>
+                      <input type="color" className="form-field is-color" />
+                    </div>
+
+                    <div className="modal-group">
+                      <p>Note more about it:</p>
+                      <textarea className="form-field" />
+                    </div>
+
+                    <a className="cta primary">Add To Calendar</a>
+                  </div>
+                </div>
+
+                <div className="modals-wrapper is-remove-modal-wrapper is-invisible">
+                  <div className="modal is-remove-modal">
+                    <p>Are you sure about deleting all entries for that day?</p>
+                    <div className="modal-btn-group">
+                      <a
+                        id="closeBtnRemoveModal"
+                        className="cta delete-secondary"
+                      >
+                        No, Go Back
+                      </a>
+                      <a className="cta delete-primary">Yes, Delete All</a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="modals-wrapper is-menu-modal-wrapper is-invisible">
+                  <div className="modal is-menu-modal">
+                    <div className="modal-close-btn">
+                      <img
+                        src="../assets/modal-x.svg"
+                        alt="Modal Close Button"
+                      />
+                    </div>
+
+                    <div className="modal-tags-group"></div>
+
+                    <div className="modal-group">
+                      <p>You noted for selectedFeeling:</p>
+                      <textarea
+                        id="infoTextArea"
+                        className="form-field"
+                        readOnly
+                      ></textarea>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Days start */}
+                {/* <div className="day-block is-top-left-corner">
+                  <div className="add-btn is-absolute">
                     <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
                   </div>
                   <div className="diary-btn-wrapper">
-                    <div className="add-btn is-add">
+                    <div className="add-btn is-add" id="openAdd">
                       <img src="../assets/green-plus.svg" alt="Green Plus" />
                     </div>
-                    <div className="add-btn is-remove">
+                    <div className="add-btn is-remove" id="openRemove">
                       <img src="../assets/red-x.svg" alt="Red X" />
                     </div>
                     <div className="add-btn is-menu">
                       <img src="../assets/blue-menu.svg" alt="Blue Menu" />
                     </div>
                   </div>
+
+                  <div className="day-tags-wrapper">
+                    <div className="tag-circles"></div>
+                    <div className="tag"></div>
+                  </div>
+
                   <div className="day-number">01</div>
-                </div>
+                </div> */}
 
-                <div className="day-block ">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">02</div>
-                </div>
+                {generateCalendar(year, new Date().getMonth()).map(
+                  (cell, index) => {
+                    let borderRadius = "";
+                    if (index === 0) borderRadius = "12px 0 0 0";
+                    if (index === 6) borderRadius = "0 12px 0 0";
+                    if (index === 28) borderRadius = "0 0 0 12px";
+                    if (index === 34) borderRadius = "0 0 12px 0";
 
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">03</div>
-                </div>
+                    return (
+                      <div
+                        key={index}
+                        className={`day-block ${
+                          cell.currentMonth ? "" : "opacity-25"
+                        }`}
+                        style={{ borderRadius }}
+                      >
+                        <div className="add-btn is-absolute">
+                          <img
+                            src="../assets/plus-non-hover.svg"
+                            alt="Plus Icon"
+                          />
+                        </div>
 
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">04</div>
-                </div>
+                        <div className="diary-btn-wrapper">
+                          <div className="add-btn is-add" id="openAdd">
+                            <img
+                              src="../assets/green-plus.svg"
+                              alt="Green Plus"
+                            />
+                          </div>
+                          <div className="add-btn is-remove" id="openRemove">
+                            <img src="../assets/red-x.svg" alt="Red X" />
+                          </div>
+                          <div className="add-btn is-menu">
+                            <img
+                              src="../assets/blue-menu.svg"
+                              alt="Blue Menu"
+                            />
+                          </div>
+                        </div>
 
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">05</div>
-                </div>
+                        <div className="day-tags-wrapper">
+                          {/* <div className="tag-circles"></div>
+                          <div className="tag"></div> */}
+                        </div>
 
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">06</div>
-                </div>
-
-                <div className="day-block is-top-right-corner">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">07</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">08</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">09</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">10</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">11</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">12</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">13</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">14</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">15</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">16</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">17</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">18</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">19</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">20</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">21</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">22</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">23</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">24</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">25</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">26</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">27</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">28</div>
-                </div>
-
-                <div className="day-block is-bottom-left-corner">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">29</div>
-                </div>
-
-                <div className="day-block">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">30</div>
-                </div>
-
-                <div className="day-block ">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">31</div>
-                </div>
-
-                <div className="day-block wrong-month">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">01</div>
-                </div>
-
-                <div className="day-block wrong-month">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">02</div>
-                </div>
-
-                <div className="day-block wrong-month">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">03</div>
-                </div>
-
-                <div className="day-block wrong-month is-bottom-right-corner">
-                  <div className="add-btn">
-                    <img src="../assets/plus-non-hover.svg" alt="Plus Icon" />
-                  </div>
-                  <div className="day-number">04</div>
-                </div>
+                        <div className="day-number">
+                          {cell.date ? String(cell.date).padStart(2, "0") : ""}
+                        </div>
+                      </div>
+                    );
+                  }
+                )}
               </div>
             </div>
           </div>
--- mediary/src/components/Profile.tsx
+++ mediary/src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "./AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { supabase } from "../models/supabaseClients";
 
 interface UserData {
@@ -15,6 +15,7 @@ function Profile() {
   const { user } = useAuth();
   const [userData, setUserData] = useState<UserData | null>(null);
   const [newsletter, setNewsletter] = useState("No");
+  const navigate = useNavigate();
 
   const fetchUser = async () => {
     const { data: userDataSupabase, error: userError } = await supabase
@@ -62,65 +63,119 @@ function Profile() {
     return <Navigate to="/login" replace />;
   }
 
+  const deleteAccount = async () => {
+    if (!user) return;
+
+    const { error: dbError } = await supabase
+      .from("users")
+      .delete()
+      .eq("id", user?.id);
+
+    if (dbError) {
+      console.error(dbError);
+      return;
+    }
+
+    await supabase.auth.signOut();
+
+    navigate("/register", { replace: true });
+  };
+
+  const deleteOpenModalBtn = document.getElementById("deleteOpenModal");
+  const deleteCloseModalBtn = document.getElementById("deleteCloseModal");
+
+  console.log(deleteCloseModalBtn);
+
+  const modalWrapper = document.getElementsByClassName(
+    "delete-modal-wrapper"
+  )[0];
+
+  deleteOpenModalBtn?.addEventListener("click", () => {
+    modalWrapper.classList.remove("is-invisible");
+  });
+
+  deleteCloseModalBtn?.addEventListener("click", () => {
+    modalWrapper.classList.add("is-invisible");
+  });
+
   return (
-    <div className="padding-global">
-      <div className="container">
-        <div className="margin-b-160">
-          <h1 className="margin-b-64">Welcome back, {userData?.name}!</h1>
+    <>
+      {" "}
+      <div className="padding-global">
+        <div className="container">
+          <div className="margin-b-160">
+            <h1 className="margin-b-64">Welcome back, {userData?.name}!</h1>
 
-          <div className="profile-wrapper">
-            <div className="profile-block">
-              <h2>
-                Your <span className="cta-color">Personal Info</span>
-              </h2>
-              <div className="info-wrapper">
-                <div className="info-block-wrapper">
-                  <span className="info-label">Email:</span>
-                  <span className="info-text">{userData?.email}</span>
-                </div>
-                <div className="info-block-wrapper">
-                  <span className="info-label">First registered:</span>
-                  <span className="info-text">{userData?.created_at}</span>
-                </div>
-                <div className="info-block-wrapper">
-                  <span className="info-label">Newsletter:</span>
-                  <span className="info-text">{newsletter}</span>
+            <div className="profile-wrapper">
+              <div className="profile-block">
+                <h2>
+                  Your <span className="cta-color">Personal Info</span>
+                </h2>
+                <div className="info-wrapper">
+                  <div className="info-block-wrapper">
+                    <span className="info-label">Email:</span>
+                    <span className="info-text">{userData?.email}</span>
+                  </div>
+                  <div className="info-block-wrapper">
+                    <span className="info-label">First registered:</span>
+                    <span className="info-text">{userData?.created_at}</span>
+                  </div>
+                  <div className="info-block-wrapper">
+                    <span className="info-label">Newsletter:</span>
+                    <span className="info-text">{newsletter}</span>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="profile-block">
-              <h2>
-                Mental Health <span className="cta-color">Statistics</span>
-              </h2>
-              <div className="info-wrapper">
-                <div className="info-block-wrapper">
-                  <span className="info-label">Most entered feeling:</span>
-                  <span className="info-text">Happy</span>
-                </div>
-                <div className="info-block-wrapper">
-                  <span className="info-label">First entered feeling:</span>
-                  <span className="info-text">Excited</span>
-                </div>
-                <div className="info-block-wrapper">
-                  <span className="info-label">Last entered feeling:</span>
-                  <span className="info-text">Sad</span>
+              <div className="profile-block">
+                <h2>
+                  Mental Health <span className="cta-color">Statistics</span>
+                </h2>
+                <div className="info-wrapper">
+                  <div className="info-block-wrapper">
+                    <span className="info-label">Most entered feeling:</span>
+                    <span className="info-text">Happy</span>
+                  </div>
+                  <div className="info-block-wrapper">
+                    <span className="info-label">First entered feeling:</span>
+                    <span className="info-text">Excited</span>
+                  </div>
+                  <div className="info-block-wrapper">
+                    <span className="info-label">Last entered feeling:</span>
+                    <span className="info-text">Sad</span>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="profile-btn-wrapper">
-              <a href="/" className="cta delete-secondary">
-                Delete All Entries
-              </a>
-              <a href="/" className="cta delete-primary">
-                Delete Account
-              </a>
+              <div className="profile-btn-wrapper">
+                <a className="cta delete-secondary">Delete All Entries</a>
+                <a className="cta delete-primary" id="deleteOpenModal">
+                  Delete Account
+                </a>
+
+                {/* Account delete modal */}
+                <div className="delete-modal-wrapper is-invisible">
+                  <div className="delete-modal">
+                    <h3 className="delete-modal-heading">
+                      Are you sure that you want to delete your account?
+                    </h3>
+                    <div className="delete-modal-btn-wrapper">
+                      <a className="cta delete-primary" onClick={deleteAccount}>
+                        Yes, Delete
+                      </a>
+
+                      <a className="cta primary" id="deleteCloseModal">
+                        No, Go Back
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
